69b33d033958195ac497dbaf81a9a0f084bf3524
shashank00818 <shashank00818@gmail.com>
2023-06-26 14:11:18 +0530

    completed views of sign in up and out

--- media_app/urls.py
+++ media_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('signup/', views.sign_up, name='sign_up'),
     path('signin/', views.sign_in, name='sign_in'),
     path('signout/', views.sign_out, name='sign_out'),
+    path('profile-settings/', views.profile_settings, name='profile_settings'),
 ]
--- media_app/views.py
+++ media_app/views.py
@@ -1,47 +1,58 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import auth
 from media_app.models import User
+from django.contrib.auth.decorators import login_required
 
 def sign_up(request):
+    page_name='sign_up.html'
     if request.method=="POST":
         username=request.POST["username"]
         email=request.POST["email"]
         password=request.POST["password"]
 
         if User.objects.filter(username=username).exists():
-            print("username already exists")
-            return redirect('signup')
+            return render(request, page_name, {"error":True,"error_msg":"User already exists"})
         
         if User.objects.filter(email=email).exists():
-            print("email already exists")
-            return redirect('signup')
+            return render(request, page_name, {"error":True,"error_msg":"Email already exists"})
         
         user=User.objects.create_user(username=username, password=password)
         user=auth.authenticate(username=username, password=password)
 
         if user:
             auth.login(request, user)
-            print("user created successfully",user)
+            return redirect('profile_settings')
         else:
-            print("some error is there")
+            return render(request, page_name, {"error":True,"error_msg":"Some error is there"})
     else:
-        print("some error is there")
+        return render(request, page_name)
 
 def sign_in(request):
     if request.method=="POST":
+        page_name='sign_in.html'
         username=request.POST["username"]
         password=request.POST["password"]
         user=auth.authenticate(username=username, password=password)
 
         if user:
             auth.login(request, user)
-            print("user is logged in", user)
+            return redirect('profile_settings')
         else:
-            print("Some error is there")
+            return render(request, page_name, {"error":True,"error_msg":"Some error"})
     else:
-        print("get method for sign in")
-        print("render sign in html page")
+        return render(request, page_name)
 
+@login_required(login_url='sign_in')
 def sign_out(request):
     auth.logout(request)
-    print("logout is done")
+    return redirect('sign_up')
+
+@login_required(login_url='sign_in')
+def profile_settings(request):
+    page_name="profile_settings.html"
+    data={
+        "id":request.user.id,
+        "email":request.user.email,
+        "username":request.user.username
+    }
+    return render(request, page_name, data)
