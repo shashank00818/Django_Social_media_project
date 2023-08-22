cd079c2458805875766603eed2612e2c3f06eab7
shashank00818 <shashank00818@gmail.com>
2023-08-22 20:49:18 +0530

    post registered

--- media_app/admin.py
+++ media_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['id','user','caption','image','created_at','updated_at']
+
+# admin.site.register(Post)
